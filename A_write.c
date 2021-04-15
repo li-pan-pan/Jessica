31d034ef2ba4aec03182b35d191ca9a2bb99730f
li-pan-pan <74539169+li-pan-pan@users.noreply.github.com>
2021-04-15 11:46:05 +0800

    Create A_write.c

---
+++ A_write.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+  2 #include<unistd.h>
+  3 #include<fcntl.h>
+  4 
+  5 int main()
+  6 {
+  7    int fd=open("_shm.c",O_RDWR);                                                                                                        
+  8    if(fd<0)
+  9    {
+ 10        perror("open");
+ 11        return 0;
+ 12    }
+ 13 
+ 14    while(1)
+ 15    {
+ 16        write(fd,"I am process A",14);
+ 17        sleep(1);
+ 18    }
+ 19    close(fd);
+ 20    return 0;
+ 21 
+ 22 }
